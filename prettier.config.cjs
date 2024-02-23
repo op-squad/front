25a74de670cf3793d298ff1c7b93bc5ec2e2dd0e
4nskarts <anasnedjmeddine@gmail.com>
2024-02-23 22:27:51 +0100

    Enforce styling rules setup tailwind css setup eslint with prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: "es5",
+
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  trailingComma: "all",
+  singleQuote: false,
+  printWidth: 80,
+  singleAttributePerLine: true,
+};
