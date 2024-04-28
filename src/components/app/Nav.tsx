04631c2b8ec3478257d6d76bd1a2692bb578775d
wb21cs <mustaben94@gmail.com>
2024-04-28 22:13:16 +0100

    fix navbar

--- src/components/app/Nav.tsx
+++ src/components/app/Nav.tsx
@@ -1,10 +1,39 @@
+import { Link } from "react-router-dom";
 export default function Nav() {
   return (
-    <div className="flex flex-col items-center text-neutral-50 space-y-16 my-32">
-      <div>Dashboard</div>
-      <div>Patients</div>
-      <div>Models</div>
-      <div>Calendar</div>
+    <div className="flex flex-col items-center text-neutral-50 my-32 w-full">
+      <div className="flex flex-col w-full">
+        <Link
+          className="py-8 text-center hover:bg-indigo-700"
+          to="/dashboard"
+        >
+          Dashboard
+        </Link>
+      </div>
+      <div className="flex flex-col w-full">
+        <Link
+          className="py-8 text-center hover:bg-indigo-700"
+          to="/patients"
+        >
+          Patients
+        </Link>
+      </div>
+      <div className="flex flex-col w-full">
+        <Link
+          className="py-8 text-center hover:bg-indigo-700"
+          to="/models"
+        >
+          Models
+        </Link>
+      </div>
+      <div className="flex flex-col w-full">
+        <Link
+          className="py-8 text-center hover:bg-indigo-700"
+          to="/calendar"
+        >
+          Calendar
+        </Link>
+      </div>
     </div>
   );
 }
