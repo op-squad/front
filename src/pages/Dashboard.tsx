0f599d5546b8f7d19da7ab0691314ff7b77aafcd
wb21cs <mustaben94@gmail.com>
2024-05-01 14:17:53 +0100

    refactor BarChart component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,104 +1,7 @@
 import Sidebar from "../components/app/Sidebar";
 import StatCard from "../components/app/dashboard/StatCard";
-// import MySvg from "../assets/stat_cards/1.svg";
 import DropdownMenu from "../components/app/DropdownMenu";
-import { useEffect, useRef } from "react";
-import * as d3 from "d3";
-
-function BarChart() {
-  const height = 340;
-  const width = 940;
-  const marginTop = 30;
-  const marginRight = 0;
-  const marginBottom = 30;
-  const marginLeft = 40;
-  const ref = useRef();
-  useEffect(() => {
-    const data = [
-      { label: "Jan", value: 512 },
-      { label: "Feb", value: 254 },
-      { label: "Mar", value: 684 },
-      { label: "Apr", value: 823 },
-      { label: "May", value: 564 },
-      { label: "Jun", value: 497 },
-      { label: "Jul", value: 185 },
-      { label: "Aug", value: 213 },
-      { label: "Sep", value: 345 },
-      { label: "Oct", value: 387 },
-      { label: "Nov", value: 488 },
-      { label: "Dec", value: 132 },
-    ];
-    const xScale = d3
-      .scaleBand()
-      .domain(data.map((e) => e.label))
-      .range([marginLeft, width - marginRight])
-      .padding(0.5);
-    const svgElement = d3.select(ref.current);
-    svgElement
-      .append("g")
-      .style("font", "24px")
-      .attr("transform", `translate(0,${height - marginBottom})`)
-      .call(d3.axisBottom(xScale))
-      .call((g) => g.select(".domain").remove())
-      .call((g) => g.selectAll(".tick line").remove())
-      .call((g) =>
-        g
-          .selectAll(".tick text")
-          .style("font-size", "14px")
-          .classed("text-gray-500", true),
-      );
-    const yScale = d3
-      .scaleLinear()
-      .domain([0, 1200])
-      .range([height - marginBottom, marginTop]);
-    svgElement
-      .append("g")
-      .attr("transform", `translate(${width}, 0)`)
-      .call(
-        d3
-          .axisLeft(yScale)
-          .tickSize(width - marginLeft - marginRight)
-          .ticks(6),
-      )
-      .call((g) => g.select(".domain").remove())
-      .call((g) =>
-        g
-          .selectAll(".tick line")
-          .attr("stroke-opacity", 0.2)
-          .attr("stroke-dasharray", "8,4"),
-      )
-      .call((g) =>
-        g
-          .selectAll(".tick text")
-          .attr("x", -width + marginRight + marginLeft - 1)
-          .attr("dy", 7)
-          .style("font-size", "14px")
-          .classed("text-gray-500", true),
-      );
-    svgElement
-      .append("g")
-      .attr("fill", "#818cf8")
-      .selectAll()
-      .data(data)
-      .join("rect")
-      .attr("x", (d) => xScale(d.label))
-      .attr("y", (d) => yScale(d.value) + 1)
-      .attr("height", (d) => yScale(0) - yScale(d.value))
-      .attr("width", xScale.bandwidth())
-      .attr("rx", 5);
-  }, []);
-  // const x = d3.scaleBand().domain(data).range();
-  // console.log(x);
-  // const y = d3.scaleLinear();
-  // console.log(y);
-  return (
-    <svg
-      ref={ref}
-      width={width}
-      height={height}
-    />
-  );
-}
+import BarChart from "../components/app/dashboard/BarChart";
 
 export default function Dashboard() {
   return (
@@ -161,17 +64,6 @@ export default function Dashboard() {
                   Patient Visits
                 </p>
                 <BarChart />
-                {/* <div className="flex px-4 pb-4 h-full">
-                  <div className="flex flex-col justify-between items-end text-blue-900 h-full pb-4">
-                    <p>1200</p>
-                    <p>1000</p>
-                    <p>800</p>
-                    <p>600</p>
-                    <p>400</p>
-                    <p>200</p>
-                    <p>0</p>
-                  </div>
-                </div> */}
               </div>
               <div className="flex flex-col gap-8 basis-1/2 bg-blue-50 rounded-xl p-8">
                 <p className="font-extrabold text-2xl text-blue-950">
