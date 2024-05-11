0ef21e0167b128e72a831f885dcf4403f92daa6f
4nskarts <an.mokhtari@esi-sba.dz>
2024-05-11 21:46:57 +0100

    forget what I did with the changes

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,3 +1,4 @@
+// import { useCreateAssistantProfileMutation } from "@/features/crud/assistant/assistantApiSlice";
 // import { useGetDoctorProfileQuery } from "@/features/crud/doctor/doctorApiSlice";
 // import { useCreateDoctorProfileMutation } from "@/features/crud/doctor/doctorApiSlice";
 // import { useDeleteDoctorProfileMutation } from "@/features/crud/doctor/doctorApiSlice";
@@ -5,10 +6,10 @@
 //   useCreatePatientMutation,
 //   useGetPatientsQuery,
 // } from "@/features/crud/patient/patientApiSlice";
-// import { useCreateAssistantProfileMutation } from "@/features/dashboard/dashboardApiSlice";
+// import { toast, useToast } from "@/utils/use-toast";
 
 // function Test() {
-//   const { data, error, isLoading } = useGetDoctorProfileQuery({}); // Pass the required arguments here
+//   const { data, error, isLoading } = useGetPatientsQuery({}); // Pass the required arguments here
 
 //   const [createDoctorProfile] = useCreateDoctorProfileMutation();
 //   const [createAssistantProfile] = useCreateAssistantProfileMutation();
@@ -44,13 +45,26 @@
 //   // deleteDoctorProfile({});
 
 //   const [createPatient] = useCreatePatientMutation();
+//   const { toast } = useToast();
 
 //   const handleCreatePatient = async () => {
 //     await createPatient({
 //       firstname: "mohamed",
 //       lastname: "bensalah",
 //       phonenumber: "0555555555",
-//     });
+//     })
+//       .unwrap()
+//       .then((res) => {
+//         console.log(res);
+//         toast({
+//           variant: "destructive",
+//           title: "Uh oh! Something went wrong.",
+//           description: "There was a problem with your request.",
+//         });
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
 //   };
 
 //   // const { data, error } = useGetPatientsQuery({}); // Pass the required arguments here
@@ -62,7 +76,6 @@
 //       onClick={handleCreatePatient}
 //     >
 //       Create patient
-//       {isLoading && <span className="ml-2">Loading...</span>}
 //       {data}
 //     </button>
 //   );
