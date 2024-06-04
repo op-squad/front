d29f07d4e7252164ff299b75701d4fe98395d9b0
Abdelouakil BENTERKI <124548980+wb21cs@users.noreply.github.com>
2024-06-04 17:51:32 +0100

    Revert "merge calendar into development"

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
+import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <Routes>
-          <Route
-            path="/*"
-            element={<App />}
-          ></Route>
-        </Routes>
-      </Provider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,19 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#000022",
-        "blue-custom": "#393DD8",
-        "gray-input": "#B9BEBF",
-        "gray-input-border": "#DEE4E6",
-      },
-    },
-    fontFamily: {
-      inter: ["Raleway", "sans-serif"],
-      Raleway: ["Raleway", "sans-serif"],
-      Lato: ["Lato", "sans-serif"],
-    },
+    extend: {},
   },
   plugins: [],
 };
